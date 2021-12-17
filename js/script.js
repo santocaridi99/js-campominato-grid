4dc023fef930672776a688e66b07b011caba763a
santocaridi99 <santocaridi99@gmail.com>
2021-12-17 16:46:12 +0100

    primo tentativo da fixare molte cose

--- js/script.js
+++ js/script.js
@@ -5,17 +5,46 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
-//creo una funzione che genera numeri (min e max)
-
 //funzione per creare i  quadratini (quando li clicco si colorano di azzurro)
-
+function createBox(containerBox){
+    const newBox = document.createElement('div')
+    newBox.className = 'box';
+    containerBox.append(newBox);
+    newBox.addEventListener('click',function(){
+        this.classList.add('azzurro');
+    })
+}
 //dichiaro variabili dei bottoni per scelta del livello
+const button1 = document.getElementById('lvl-1');
+const button2 = document.getElementById('lvl-2');
+const button3 = document.getElementById('lvl-3');
+//dichiaro container
+const container = document.querySelector('.container');
 //se utente sceglierà livello 1
 //genera numeri da 1 a 100
 //aggiungi classe container in modo tale che entrano 100 quadratini
+button1.addEventListener('click',function(){
+    for(let i=0 ; i < 100 ; i++){
+        createBox(container)
+    }
+    container.classList.toggle('easy-container');
+    
+})
 //se utente sceglie livello 2 
 //genera numeri  da 1 a 81
 //aggiungi classe container in modo tale che entrano 81 quadratini
+button2.addEventListener('click',function(){
+    for(let i=0 ; i < 81 ; i++){
+        createBox(container)
+    }
+    container.classList.toggle('medium-container');
+})
 //se utente sceglie livello 3
 //genera numeri  da 1 a 49
 //aggiungi classe container in modo tale che entrano 49 quadratini
+button3.addEventListener('click',function(){
+    for(let i=0 ; i < 49 ; i++){
+        createBox(container)
+    }
+    container.classList.toggle('hard-container');
+})
