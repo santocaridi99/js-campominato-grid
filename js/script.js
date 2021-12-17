cbd2c9eff9eb262e4e1b557c1ae138a0ab31d2d4
santocaridi99 <santocaridi99@gmail.com>
2021-12-17 15:16:34 +0100

    creazione file  e aggiunto commenti (primo ragionamento)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+L’utente indica un livello di difficoltà (3 pulsanti) in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+//creo una funzione che genera numeri (min e max)
+
+//funzione per creare i  quadratini (quando li clicco si colorano di azzurro)
+
+//dichiaro variabili dei bottoni per scelta del livello
+//se utente sceglierà livello 1
+//genera numeri da 1 a 100
+//aggiungi classe container in modo tale che entrano 100 quadratini
+//se utente sceglie livello 2 
+//genera numeri  da 1 a 81
+//aggiungi classe container in modo tale che entrano 81 quadratini
+//se utente sceglie livello 3
+//genera numeri  da 1 a 49
+//aggiungi classe container in modo tale che entrano 49 quadratini
